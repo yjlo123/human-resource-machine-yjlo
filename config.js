0e55acfcf27e9e1bfacd762774a48669bf538c86
yjlo123 <liusiwei.yjlo@gmail.com>
2017-10-25 00:06:28 +0800

    Added level menu data

--- config.js
+++ config.js
@@ -4,3 +4,5 @@ CONFIG = {
 		"human-resource-machine-yjlo": "../"
 	}
 };
+
+level_menu_list = [{"url": "Level_01.yjlo", "title": "Level 01"}, {"url": "Level_02.yjlo", "title": "Level 02"}, {"url": "Level_03.yjlo", "title": "Level 03"}, {"url": "Level_04.yjlo","title": "Level 04"}, {"url": "Level_05.yjlo", "title": "Level 05"}, {"url": "Level_06.yjlo", "title": "Level 06"}, {"url": "Level_07.yjlo", "title": "Level 07"}, {"url": "Level_08.yjlo", "title": "Level 08"}, {"url": "Level_09.yjlo", "title": "Level 09"}, {"url": "Level_10.yjlo", "title": "Level 10"}, {"url": "Level_11.yjlo", "title": "Level 11"}, {"url": "Level_12.yjlo", "title": "Level 12"}, {"url": "Level_13.yjlo", "title": "Level 13"}, {"url": "Level_14.yjlo", "title": "Level 14"}, {"url": "Level_15.yjlo", "title": "Level 15"}, {"url": "Level_16.yjlo", "title": "Level 16"}, {"url": "Level_17.yjlo", "title": "Level 17"}, {"url": "Level_18.yjlo", "title": "Level 18"}, {"url": "Level_19.yjlo", "title": "Level 19"}, {"url": "Level_20.yjlo", "title": "Level 20"}];
